c2505245ca7a61655b7f1e8fcbd40cb077e7b0e5
Ben Pennell <bbpennel@email.unc.edu>
2023-04-12 12:38:21 -0400

    debug

--- server/lib/deposit/submit-zip.js
+++ server/lib/deposit/submit-zip.js
@@ -37,7 +37,7 @@ function makeZip(submission) {
           zip.addFile(name, submission[name]);
           logging.error("makeZip archive append buffer " + name);
         } else {
-          content = fs.readFileSync(submission[name]);
+          const content = fs.readFileSync(submission[name]);
           zip.addFile(name, content);
           // zip.addLocalFile(submission[name], name);
           logging.error("makeZip archive append " + name + " | " + submission[name]);
